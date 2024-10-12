62da108ea550472838f7c8ffc1a274cf664c0570
jmmarketing <jeff@jeffreymclean.com>
2024-10-11 18:34:17 -0700

    Reworking error-message logic in js. Working, but needs to be cleaned up.

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -713,33 +713,39 @@ function validateInputs() {
     // First validation makes sure there is a number and not blank
     ageInputs.forEach((input)=>{
         const val = Number.parseInt(+input.value);
+        console.log(val);
         if (!Number.isInteger(val) || !input.value) {
             input.classList.add("invalid");
+            input.nextElementSibling.textContent = "This field is required";
             birthday[input.name] = null;
         } else {
             birthday[input.name] = val;
             input.classList.remove("invalid");
         }
     });
+    if (Object.values(birthday).includes(null)) return;
     // Second pass of validation is field specific, but some requires other field info (like days for days in month validation)
     if (birthday.year > rawToday.year) {
         yearInput.classList.add("invalid");
+        yearInput.nextElementSibling.textContent = "Must be in the past";
         birthday.year = null;
         return;
     }
     if (birthday.month < 1 || birthday.month > 12) {
         monthInput.classList.add("invalid");
+        monthInput.nextElementSibling.textContent = "Must be a valid month";
+        birthday.month = null;
         return;
     }
     if (birthday.day <= 0 || birthday.day > daysOfMonth.get(birthday.month)) {
-        dayInput.classList.add("invalid");
+        ageInputs.forEach((input)=>input.classList.add("invalid"));
+        dayInput.nextElementSibling.textContent = "Must be a valid date";
+        monthInput.nextElementSibling.textContent = "";
+        yearInput.nextElementSibling.textContent = "";
         birthday.day = null;
         return;
     }
-    if (!(0, _dateFns.isBefore)(`${birthday.year}/${birthday.month}/${birthday.day}`, `${rawToday.year}/${rawToday.month}/${rawToday.day}`)) {
-        ageInputs.forEach((input)=>input.classList.add("invalid"));
-        return;
-    }
+    if (!(0, _dateFns.isBefore)(`${birthday.year}/${birthday.month}/${birthday.day}`, `${rawToday.year}/${rawToday.month}/${rawToday.day}`)) ageInputs.forEach((input)=>input.classList.add("invalid"));
 }
 const submitButton = document.querySelector(".calculator__divider--icon");
 const dayInput = document.querySelector("#day");
